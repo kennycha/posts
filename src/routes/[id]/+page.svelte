f6a1ee921c60492043ab1b8d67edf139725928a9
kennycha <dudqn136@naver.com>
2023-08-09 16:31:53 +0900

    feat: remove ssr

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,9 +1,26 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import SvelteMarkdown from 'svelte-markdown';
+	import { page } from '$app/stores';
 	import './markdown.css';
-	export let data;
+
+	let source: string;
+	let hasError = false;
+
+	onMount(async () => {
+		const res = await fetch(`posts/${$page.params.id}.md`);
+		if (res.status === 404) {
+			hasError = true;
+		} else {
+			source = await res.text();
+		}
+	});
 </script>
 
-<div id="markdown">
-	<SvelteMarkdown source={data.source} />
-</div>
+{#if hasError}
+	<div class="error">404: not found</div>
+{:else}
+	<div id="markdown">
+		<SvelteMarkdown {source} />
+	</div>
+{/if}
